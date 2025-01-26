e1a8e31b3b045c79b1a78ddacac0d131679acdd0
seansanielsolis2131 <lr.sssolis@mmdc.mcl.edu.ph>
2025-01-26 08:52:39 +0800

    Add files via upload

---
+++ connectly_project/posts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from django.urls import path
+from .views import UserListCreate, PostListCreate, CommentListCreate
+
+urlpatterns = [
+    path('users/', UserListCreate.as_view(), name='user-list-create'),
+    path('posts/', PostListCreate.as_view(), name='post-list-create'),
+    path('comments/', CommentListCreate.as_view(), name='comment-list-create'),
+]
